7731f83eb76dfc86b534a0df41c9cb0bfcaca92d
kholostyak17 <kholostyak17@gmail.com>
2021-03-29 19:49:50 +0200

    nuevos cambios

---
+++ app.js
@@ -0,0 +1,12 @@
+function onLoad(){
+    
+    let who = ['the dog','my granma','his turtle','my bird'];
+    let what = ['eat','pissed','crushed','broked'];
+    let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+    var resultado;
+    
+    return resultado;
+}
+
+
+console.log
