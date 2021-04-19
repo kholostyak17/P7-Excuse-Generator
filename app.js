438b1396a6ba2a112936d6a9b2a3c23ae0f2902a
kholostyak17 <kholostyak17@gmail.com>
2021-04-19 14:08:46 +0000

    improved js function

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+function numAleatorio(myArray){
+    return myArray[Math.floor(Math.random()*myArray.length)];
+}
+
 function onLoad() {
-
-    let random1 = Math.random();
-    let random2 = Math.random();
-    let random3 = Math.random();
-
     let who = ['El gato', 'Mi hermana', 'Mi amigo Ramón', 'Mi novia'];
     let what = ['me ha tirado', 'me ha quemado', 'me ha robado', 'me ha perdido'];
     let when = ['ayer por la noche', 'hace un mes', 'justo cuando desperté', 'mientras estaba en la ducha', 'mientras alimentaba al pájaro'];
-    let excusa = (who[Math.floor(random1 * who.length)] + ' ' + what[Math.floor(random2 * what.length)] + ' mis ejercicios ' + when[Math.floor(random3 * when.length)] + '. :(');
-
+    let excusa = (numAleatorio(who) + ' ' + numAleatorio(what) + ' mis ejercicios ' + numAleatorio(when) + '. :(');
     return excusa;
 }
 
