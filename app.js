a2d6e5b45731e8b3f465205fc02a3224f9a41122
kholostyak17 <kholostyak17@gmail.com>
2021-04-19 14:34:41 +0000

    replaced ' to "

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ function numAleatorio(myArray){
 }
 
 function onLoad() {
-    let who = ['El gato', 'Mi hermana', 'Mi amigo Ramón', 'Mi novia'];
-    let what = ['me ha tirado', 'me ha quemado', 'me ha robado', 'me ha perdido'];
-    let when = ['ayer por la noche', 'hace un mes', 'justo cuando desperté', 'mientras estaba en la ducha', 'mientras alimentaba al pájaro'];
-    let excusa = (numAleatorio(who) + ' ' + numAleatorio(what) + ' mis ejercicios ' + numAleatorio(when) + '. :(');
+    let who = ["El gato", "Mi hermana", "Mi amigo Ramón", "Mi novia"];
+    let what = ["me ha tirado", "me ha quemado", "me ha robado", "me ha perdido"];
+    let when = ["ayer por la noche", "hace un mes", "justo cuando desperté", "mientras estaba en la ducha", "mientras alimentaba al pájaro"];
+    let excusa = (numAleatorio(who) + " " + numAleatorio(what) + " mis ejercicios " + numAleatorio(when) + ". :(');
     return excusa;
 }
 
-console.log('hola!!!');
+console.log("hola!!!");
 document.querySelector('#excusa').innerHTML = onLoad();
