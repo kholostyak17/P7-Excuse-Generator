59e547ab30ea0d12e7aacebef5c56342efcbc165
kholostyak17 <kholostyak17@gmail.com>
2021-04-20 10:11:18 +0000

    changed + to .concat

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-function numAleatorio(myArray){
-    return myArray[Math.floor(Math.random()*myArray.length)];
+function numAleatorio(myArray) {
+    return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 function onLoad() {
     let who = ["El gato", "Mi hermana", "Mi amigo Ramón", "Mi novia"];
     let what = ["me ha tirado", "me ha quemado", "me ha robado", "me ha perdido"];
     let when = ["ayer por la noche", "hace un mes", "justo cuando desperté", "mientras estaba en la ducha", "mientras alimentaba al pájaro"];
-    let excusa = (numAleatorio(who) + " " + numAleatorio(what) + " mis ejercicios " + numAleatorio(when) + ". :(");
+    let excusa = (numAleatorio(who).concat(" ".concat(numAleatorio(what).concat(" mis ejercicios ".concat(numAleatorio(when).concat(". :("))))));
     return excusa;
 }
 
