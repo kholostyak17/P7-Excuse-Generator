f264b5c51b466a398746329cfa39d971de54fe9c
kholostyak17 <kholostyak17@gmail.com>
2021-04-21 16:29:19 +0000

    fixed .concat sucession

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function onLoad() {
     let who = ["El gato", "Mi hermana", "Mi amigo Ramón", "Mi novia"];
     let what = ["me ha tirado", "me ha quemado", "me ha robado", "me ha perdido"];
     let when = ["ayer por la noche", "hace un mes", "justo cuando desperté", "mientras estaba en la ducha", "mientras alimentaba al pájaro"];
-    let excusa = (numAleatorio(who).concat(" ".concat(numAleatorio(what).concat(" mis ejercicios ".concat(numAleatorio(when).concat(". :("))))));
+    let excusa = ("".concat(numAleatorio(who)," ",numAleatorio(what)," mis ejercicios ",numAleatorio(when),"... :("));
     return excusa;
 }
 
