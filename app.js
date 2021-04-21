231e6187c798ce6af02836eb72ac9aa034885527
kholostyak17 <kholostyak17@gmail.com>
2021-04-21 16:42:52 +0000

    excluded arrays out of the function & edited to const

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+const who = ["El gato", "Mi hermana", "Mi amigo Ramón", "Mi novia"];
+const what = ["me ha tirado", "me ha quemado", "me ha robado", "me ha perdido"];
+const when = ["ayer por la noche", "hace un mes", "justo cuando desperté", "mientras estaba en la ducha", "mientras alimentaba al pájaro"];
+
 function numAleatorio(myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 function onLoad() {
-    let who = ["El gato", "Mi hermana", "Mi amigo Ramón", "Mi novia"];
-    let what = ["me ha tirado", "me ha quemado", "me ha robado", "me ha perdido"];
-    let when = ["ayer por la noche", "hace un mes", "justo cuando desperté", "mientras estaba en la ducha", "mientras alimentaba al pájaro"];
+
     let excusa = ("".concat(numAleatorio(who), " ", numAleatorio(what), " mis ejercicios ", numAleatorio(when), "... :("));
     return excusa;
 }
@@ -13,7 +15,7 @@ function onLoad() {
 window.onload = () => {
     console.log("hola!!! (funciona script JS)");
     document.querySelector('#excusa').innerHTML = onLoad();
-    document.querySelector('#generarExcusa').addEventListener("click", ()=>{
+    document.querySelector('#generarExcusa').addEventListener("click", () => {
         document.querySelector('#excusa').innerHTML = onLoad();
     });
 }
