f3237bac2493aee41892a036450a9cc6a2047c8d
kholostyak17 <kholostyak17@gmail.com>
2021-04-23 17:05:48 +0000

    spanish to english & changed function position

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const who = ["El gato", "Mi hermana", "Mi amigo Ramón", "Mi novia"];
 const what = ["me ha tirado", "me ha quemado", "me ha robado", "me ha perdido"];
 const when = ["ayer por la noche", "hace un mes", "justo cuando desperté", "mientras estaba en la ducha", "mientras alimentaba al pájaro"];
 
-function numAleatorio(myArray) {
+window.onload = () => {
+    console.log("hola!!! (funciona script JS)");
+    document.querySelector('#excuse').innerHTML = Generate();
+    document.querySelector('#generateExcuse').addEventListener("click", () => {
+        document.querySelector('#excuse').innerHTML = Generate();
+    });
+}
+
+function randomNumber(myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-function onLoad() {
-
-    let excusa = ("".concat(numAleatorio(who), " ", numAleatorio(what), " mis ejercicios ", numAleatorio(when), "... :("));
-    return excusa;
+function Generate() {
+    let excuse = ("".concat(randomNumber(who), " ", randomNumber(what), " mis ejercicios ", randomNumber(when), "... :("));
+    return excuse;
 }
 
-window.onload = () => {
-    console.log("hola!!! (funciona script JS)");
-    document.querySelector('#excusa').innerHTML = onLoad();
-    document.querySelector('#generarExcusa').addEventListener("click", () => {
-        document.querySelector('#excusa').innerHTML = onLoad();
-    });
-}
+
